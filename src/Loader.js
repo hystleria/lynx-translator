0defb1fa6a4820620e244f8b0762174758f76e47
Archie McVey <archie@laby.gg>
2021-08-07 16:39:04 +0100

    manually load translation files

--- src/Loader.js
+++ src/Loader.js
@@ -1,31 +1,20 @@
-const { readdirSync } = require('fs');
-
 module.exports = class Loader {
     constructor(translations) {
         this.translations = translations;
+
+        this.langs = ['en-UK', 'fi-FI'];
         this.loaded = false;
     }
 
     /**
-     * Loads all translations
+     * Loads translations
 	 * @returns {*}
 	 */
-    loadAll() {
-        const translations = readdirSync('./translations');
-        translations.filter((t) => t.split('.').pop() == 'json').forEach((file) => {
-            this.load(file.split('.')[0]);
-            this.loaded = true;
-        });
-    }
-
-    /**
-     * Loads a translation
-     * @param {String} lang Language code
-	 * @returns {*}
-	 */
-    load(lang) {
-        const translation = require(`../translations/${lang}.json`);
+    load() {
+        for (const lang of langs) {
+            const translation = require(`../translations/${lang}.json`);
         
-        this.translations.set(lang, translation);
+            this.translations.set(lang, translation);
+        }
     }
 };
